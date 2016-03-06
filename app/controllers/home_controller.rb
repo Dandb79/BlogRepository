85862fb132d296201d7cf26b1a80b92a871403c2
Dandb79 <danieldeburgo@gmail.com>
2016-03-06 15:30:20 +0000

    Home page function massive VICTORY!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @posts = Dir.entries("/home/vp/Desktop/blog/app/views/posts")
+    @posts = @posts.select { |x| x.end_with?(".html.erb")}
+    @posts = @posts.map { |x| x.sub! ".html.erb", ""}
   end
 end
